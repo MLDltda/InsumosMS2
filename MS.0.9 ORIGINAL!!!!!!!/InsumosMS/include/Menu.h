5369a7fa5ba65997b6f61de85ba9fe814e3877f6
Felipe-Rod-Ass <84815453+Felipe-Rod-Ass@users.noreply.github.com>
2021-06-13 22:02:35 -0300

    Add files via upload

---
+++ MS.0.9 ORIGINAL!!!!!!!/InsumosMS/include/Menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "Controler.h"
+
+class Menu : public Controler
+{
+    public:
+        Menu();
+        void exibeMenu1();
+        virtual ~Menu();
+
+    protected:
+
+    private:
+};
+
+#endif // MENU_H
